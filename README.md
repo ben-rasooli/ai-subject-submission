73e4bb5e6f12b40a339136e06e1c51f7056dd3c9
ben-rasooli <ben.rasooli@gmail.com>
2019-07-29 10:55:24 +1000

    Add an empty project - including MathLib and DataStructureLib

---
+++ README.md
@@ -0,0 +1,2 @@
+# 2D-game-using-math-library
+A project to demonstrate using the custom math library which was build during Math For Game subject
---
+++ project2D/Game2D.cpp
@@ -0,0 +1,73 @@
+#include "Game2D.h"
+
+#include "Application.h"
+#include "Texture.h"
+#include "Font.h"
+#include "Input.h"
+
+Game2D::Game2D(const char* title, int width, int height, bool fullscreen) : Game(title, width, height, fullscreen)
+{
+	// Initalise the 2D renderer.
+	m_2dRenderer = new aie::Renderer2D();
+
+	// Create some textures for testing.
+	m_font = new aie::Font("./font/consolas.ttf", 18);
+
+	// Create a player object.
+}
+
+Game2D::~Game2D()
+{
+	// Delete player.
+
+	// Deleted the textures.
+	delete m_font;
+
+	// Delete the renderer.
+	delete m_2dRenderer;
+}
+
+void Game2D::Update(float deltaTime)
+{
+	// Update the player.
+
+	// Input example: Update the camera position using the arrow keys.
+	aie::Input* input = aie::Input::GetInstance();
+
+	// Exit the application if escape is pressed.
+	if (input->IsKeyDown(aie::INPUT_KEY_ESCAPE))
+	{
+		aie::Application* application = aie::Application::GetInstance();
+		application->Quit();
+	}
+}
+
+void Game2D::Draw()
+{
+	aie::Application* application = aie::Application::GetInstance();
+	float time = application->GetTime();
+
+	// Wipe the screen to clear away the previous frame.
+	application->ClearScreen();
+
+	// Prepare the renderer. This must be called before any sprites are drawn.
+	m_2dRenderer->Begin();
+
+	// Draw the player.
+
+	// Demonstrate animation.
+	float animSpeed = 10.0f;
+	int frame = ((int)(time * animSpeed) % 6);
+	float size = 1.0f / 6.0f;
+	m_2dRenderer->SetUVRect(frame * size, 0.0f, size, 1.0f);
+	m_2dRenderer->SetUVRect(0.0f, 0.0f, 1.0f, 1.0f);
+	
+	// Draw some text.
+
+	m_2dRenderer->SetRenderColour(1.0f, 1.0f, 1.0f, 0.5f);
+	float windowHeight = (float)application->GetWindowHeight();
+	m_2dRenderer->DrawText2D(m_font, "Press ESC to quit!", 15.0f, windowHeight - 25.0f);
+
+	// Done drawing sprites. Must be called at the end of the Draw().
+	m_2dRenderer->End();
+}
